417bc305b9aa2de5a70c900193cfe70718390e2c
LIULEI <43295453+xiaoyaoll@users.noreply.github.com>
2020-11-04 08:29:53 -0600

    Update ConfigApplication.java

--- springcloud2020/config/src/main/java/com/xiaoyao/config/ConfigApplication.java
+++ springcloud2020/config/src/main/java/com/xiaoyao/config/ConfigApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigApplication {
-
+//刘磊
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
 	}
